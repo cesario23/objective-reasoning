c40026e075b05045f1148e4862ac1995c0235b90
cesar juarez <cesar.juarez@codeimmersives.com>
2021-03-11 09:13:08 -0800

    code

--- main.js
+++ main.js
@@ -1,8 +1,45 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
+function isAdmin (user){
+    if (user.userRole === "ADMIN"){
+        return true;
+    }else {
+        return false;
+    }
+}
 
+function getEmail (user){
+const lowerFirst = user.firstName.toLowerCase();
+const lowerLast = user.lastName.toLowerCase ();
+return ${lowerFirst}.${lowerLast}"@codeimmersives.com";
+}
 
+function getPlaylistLength (playlistObject){
+   return playlistObject.songs.length;
+}
+
+function  getHardestHomework (homeworks){
+   let smallest = 100;
+   let nameOfHardest = '';
+   
+    for (const item of homeworks){
+        if (item.averageScore < smallest){
+            nameOfHardest = item.name;
+            smallest = item.averageScore;
+        }
+
+    }
+return nameOfHardest;
+}
+
+function createPhonebook (names, numbers){
+    const phonebook = {};
+    for (let i = 0; i < names.length; i++){
+        phonebook [names[i]] = numbers[i];
+    }
+    return phonebook;
+}
 
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
